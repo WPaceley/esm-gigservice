125cfa832a6f4ec774afbbe2412b5fe47fd1ce4c
WPaceley <wpaceley@ns44.esm.rochester.edu>
2014-07-22 16:40:45 -0400

    Added New Client Homepage
    Added a menu to the client home page.

--- page-client-menu.php
+++ page-client-menu.php
@@ -5,32 +5,28 @@ if( !defined( 'ABSPATH' ) ) {
 }
 
 /*
-Template Name: Client Home
+Template Name: Client Home with Menu
 Author: William Paceley
 Author URL: http://arpegg.io
 */
 
 get_header(); ?>
 
-<div id="content-full" class="grid col-940">
+<div id="content" class="grid col-620">
 
 	<?php if( have_posts() ) : ?>
-		<!--WP loop starts here-->
+		
+        <!--WP Loop Starts Here-->
 		<?php while( have_posts() ) : the_post(); ?>
-			<!--Provides breadcrumbs back to landing page, so turning OFF-->
-			<?php //get_template_part( 'loop-header' ); ?>
 
 			<?php responsive_entry_before(); ?>
-			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php responsive_entry_top(); ?>
-				
-                <!--Currently this handles the title of the page, OFF for Gig Service-->
-				<?php //get_template_part( 'post-meta-page' ); ?>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>
+				<?php responsive_entry_top(); ?>>
 
 				<div class="post-entry">
-                	<!--This will display any content typed into the page-->
-					<?php the_content( __( 'Read more &#8250;', 'responsive' ) ); ?>
-                    <h2>Your Profile</h2>
+                	<!--This would display all body content typed in--turned off for this page-->
+					<?php //the_content( __( 'Read more &#8250;', 'responsive' ) ); ?>
+					<h2>Your Profile</h2>
                     <div id="client-profile">
                     	<?php
     						$current_user = wp_get_current_user();
@@ -62,35 +58,9 @@ get_header(); ?>
 							}
 						?>
                         <br /><a href="http://www.esm.rochester.edu/iml/blog/edit-client-profile/ ">Edit Profile</a>
-                        <hr>
-                    </div>
-                    <div id="client-gigs">
-                    <h2>Your Gigs</h2>
-                    <table id="gig table">
-                    <?php
-					    global $current_user;
-    					get_currentuserinfo();
-    					$author_query = array('posts_per_page' => '-1','author' => $current_user->ID);
-    					$author_posts = new WP_Query($author_query);
-    					while($author_posts->have_posts()) : $author_posts->the_post();
-    				?>
-                    <!--WP Loop starts here-->
-					<tr>
-  						<td><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></td>
-  						<td>Posted <?php the_time( 'F j, Y' );?></td>
-                        <?php 
-						if (post_status != 'draft') {
-							echo '<td>Active Listing</td>';
-						}
-						?>		
-  						<td><?php show_delete_button() ?></td>
-					</tr>
-                    <?php endwhile; ?>
-                    </table>
                     </div>
 				</div>
 				<!-- end of .post-entry -->
-
 				<?php get_template_part( 'post-data' ); ?>
 
 				<?php responsive_entry_bottom(); ?>
@@ -113,6 +83,32 @@ get_header(); ?>
 	endif;
 	?>
 
-</div><!-- end of #content-full -->
+</div><!-- end of #content -->
 
+<?php get_sidebar( 'right' ); ?>
+<hr>
+<div id="client-gigs">
+	<h2>Your Gigs</h2>
+    <table id="gig table">
+    	<?php
+			global $current_user;
+    		get_currentuserinfo();
+    		$author_query = array('posts_per_page' => '-1','author' => $current_user->ID);
+    		$author_posts = new WP_Query($author_query);
+    		while($author_posts->have_posts()) : $author_posts->the_post();
+    	?>
+        <!--WP Loop starts here-->
+		<tr>
+  			<td><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></td>
+  			<td>Posted <?php the_time( 'F j, Y' );?></td>
+            <?php 
+				if (post_status != 'draft') {
+					echo '<td>Active Listing</td>';
+				}
+			?>		
+  			<td><?php show_delete_button() ?></td>
+		</tr>
+        <?php endwhile; ?>
+        </table>
+</div>
 <?php get_footer(); ?>
